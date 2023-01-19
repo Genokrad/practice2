177dd1fd575aeaa85f56fb0fc2f306382f887917
Genokrad <zeeper90@gmail.com>
2023-01-19 15:14:53 +0200

    3rd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { UsersList } from './UsersList/UsersList';
+import { Section } from './Section/Section';
+import { data } from '../data/users';
+
+export class App extends Component {
+  state = {
+    users: data,
+  };
+
+  deleteUser = userId => {
+    this.setState(prevState => {
+      return {
+        users: prevState.users.filter(user => user.id !== userId),
+      };
+    });
+  };
+
+  changeStatus = userId => {
+    this.setState(prevState => {
+      return {
+        users: prevState.users.map(user => {
+          if (user.id === userId) {
+            return { ...user, hasJob: !user.hasJob };
+          } else {
+            return user;
+          }
+        }),
+      };
+    });
+  };
+
+  render() {
+    const { users } = this.state;
+    return (
+      <>
+        <Section>
+          <UsersList
+            users={users}
+            deleteUser={this.deleteUser}
+            changeStatus={this.changeStatus}
+          />
+        </Section>
+      </>
+    );
+  }
+}
