9b1dce9581da13ea344d3e36f6d1adc6f4ca1346
Genokrad <zeeper90@gmail.com>
2023-01-23 14:05:22 +0200

    all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { UsersList } from './UsersList/UsersList';
 import { Section } from './Section/Section';
 import { data } from '../data/users';
+import { Button } from './Button/Button';
+import { AddForm } from './AddForm/AddForm';
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
     users: data,
+    isFormShown: false,
   };
 
   deleteUser = userId => {
@@ -30,8 +34,29 @@ export class App extends Component {
     });
   };
 
+  openForm = () => {
+    this.setState({ isFormShown: true });
+  };
+
+  closeForm = () => {
+    this.setState({ isFormShown: false });
+  };
+
+  addUser = data => {
+    const newUser = {
+      id: nanoid(),
+      hasJob: false,
+      ...data,
+    };
+    this.setState(prevState => {
+      return {
+        users: [...prevState.users, newUser],
+      };
+    });
+  };
+
   render() {
-    const { users } = this.state;
+    const { users, isFormShown } = this.state;
     return (
       <>
         <Section>
@@ -40,6 +65,11 @@ export class App extends Component {
             deleteUser={this.deleteUser}
             changeStatus={this.changeStatus}
           />
+          {isFormShown ? (
+            <AddForm addUser={this.addUser} closeForm={this.closeForm} />
+          ) : (
+            <Button text="Add User" clickHandler={this.openForm} />
+          )}
         </Section>
       </>
     );
